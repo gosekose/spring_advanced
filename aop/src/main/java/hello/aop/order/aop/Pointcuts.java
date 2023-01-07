092f7e8e953be7e43c16ca9115ce1b4830281618
gosekose <ksyn1611@naver.com>
2023-01-08 03:18:24 +0900

    feat: aop around, point컷 분리 around, point컷 분리, pointcut 우선순위 작성 필요

---
+++ aop/src/main/java/hello/aop/order/aop/Pointcuts.java
@@ -0,0 +1,25 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+public class Pointcuts {
+
+    @Pointcut("execution(* hello.aop.order..*(..))")
+    public void allOrder(){}
+
+    // 클래스 이름 패턴이 *Service
+    @Pointcut("execution(* *..*Service.*(..))")
+    public void allService(){}
+
+
+    //allOrder && allService
+    @Pointcut("allOrder() && allService()")
+    public void orderAndService() {}
+
+
+}
---
+++ aop/src/test/java/hello/aop/order/AopTest.java
@@ -0,0 +1,46 @@
+package hello.aop.order;
+
+import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(AspectV4Pointcut.class)
+@SpringBootTest
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void aopInfo() throws Exception {
+        //given
+        log.info("isAopProxy orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    public void success() throws Exception {
+
+        orderService.orderItem("itemA");
+
+    }
+
+    @Test
+    void exception() {
+        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+}
